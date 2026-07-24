53e8472844953a593a5d1875282e9100109d1bd8
j0eTheRipper <j0etheripper0010@gmail.com>
2026-07-24 15:50:46 +0800

    Fixed all errors

---
+++ WebApplication1/Migrations/20260724074156_AddHexColorAndHue.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHexColorAndHue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HexColor",
+                table: "photos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Hue",
+                table: "photos",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HexColor",
+                table: "photos");
+
+            migrationBuilder.DropColumn(
+                name: "Hue",
+                table: "photos");
+        }
+    }
+}
